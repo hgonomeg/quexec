0c47cb5d42f3ad4d55e7360076ca8c5385c975a1
hgonomeg <hgonomeg@gmail.com>
2022-07-24 05:38:31 +0200

    Add support for configurable parallelism and pipe input

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,60 @@
-use anyhow::{bail, Context};
-use std::process::Stdio;
+//use anyhow::{bail, Context};
+use clap::Parser;
+use std::{process::Stdio, sync::Arc};
 use tokio::{
     fs,
     io::{AsyncBufRead, AsyncBufReadExt, BufReader},
     process::Command,
     runtime,
+    sync::{OwnedSemaphorePermit, Semaphore},
 };
 
-async fn runner(command: &str) {
+/// Simple utility to queue commands to be run
+#[derive(Parser, Debug)]
+#[clap(version, about, long_about = None)]
+struct Config {
+    /// Max number of parallel jobs
+    #[clap(short, long, default_value = "1")]
+    jobs: u16,
+    #[clap(index = 1)]
+    filename: Option<String>,
+}
+
+#[derive(Clone)]
+struct RunnerConfig {
+    inherit_output: bool,
+    inherit_input: bool,
+}
+
+impl From<Config> for RunnerConfig {
+    fn from(c: Config) -> Self {
+        Self {
+            inherit_input: c.filename.is_some() && c.jobs == 1,
+            inherit_output: c.jobs == 1,
+        }
+    }
+}
+
+async fn runner(command: String, cfg: RunnerConfig, perm: OwnedSemaphorePermit) {
     let parts: Vec<&str> = command.split_ascii_whitespace().collect();
     let mut command = Command::new(parts[0]);
+    let output_stdio = || {
+        if cfg.inherit_output {
+            Stdio::inherit()
+        } else {
+            Stdio::null()
+        }
+    };
     command
         .args(&parts[1..])
-        .stdin(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit());
+        .kill_on_drop(false)
+        .stdin(if cfg.inherit_input {
+            Stdio::inherit()
+        } else {
+            Stdio::null()
+        })
+        .stderr(output_stdio())
+        .stdout(output_stdio());
     if let Err(e) = command.status().await {
         eprintln!(
             "Error running child process \"{}\" with args {:#?}: {}",
@@ -23,23 +63,32 @@ async fn runner(command: &str) {
             e
         );
     }
+    drop(perm);
 }
 
 fn main() -> anyhow::Result<()> {
-    if let Some(filename) = std::env::args().skip(1).next() {
-        let rt = runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()?;
-        rt.block_on(async {
-            let input = fs::OpenOptions::new().read(true).open(&filename).await?;
-            let reader = BufReader::new(input);
-            let mut lines = reader.lines();
-            while let Some(line) = lines.next_line().await? {
-                runner(&line).await;
-            }
-            Ok(())
-        })
-    } else {
-        bail!("No input file given!")
-    }
+    let cfg = Config::parse();
+    let rt = runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?;
+    rt.block_on(async move {
+        let input = fs::OpenOptions::new()
+            .read(true)
+            .open(
+                &cfg.filename
+                    .as_ref()
+                    .map(String::as_str)
+                    .unwrap_or("/dev/stdin"),
+            )
+            .await?;
+        let reader = BufReader::new(input);
+        let mut lines = reader.lines();
+        let semaphore = Arc::from(Semaphore::new(cfg.jobs as usize));
+        let rt_conf = RunnerConfig::from(cfg);
+        while let Some(line) = lines.next_line().await? {
+            let perm = semaphore.clone().acquire_owned().await.unwrap();
+            tokio::spawn(runner(line.clone(), rt_conf.clone(), perm));
+        }
+        Ok(())
+    })
 }
